dac9548442a5e60a783426d121b77e4aedbf782c
kodak-code <matiasmamani163@gmail.com>
2025-02-04 17:25:21 -0300

    creacion de las vistas

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,15 @@
+<h1 class="nombre-pagina">Olvide Password</h1>
+<p class="descripcion-pagina">Reestablece tu password escribiendo tu email a continuacion</p>
+
+<form action="/olvide" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Tu email">
+    </div>
+
+    <input type="submit" value="Recuperar Password" class="boton">
+</form>
+<div class="acciones">
+    <a href="/">¿Ya tenes una cuenta? ¡Inicia Sesion!</a>
+    <a href="/crear-cuenta">¿Aun no tenes una Cuenta?</a>
+</div>
