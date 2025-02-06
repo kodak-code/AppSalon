e217d0e4cbaebcc642908949aeb73979593de7e0
kodak-code <matiasmamani163@gmail.com>
2025-02-06 16:41:10 -0300

    autenticacion de usuarios

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,63 +1,112 @@
-<?php 
+<?php
+
 namespace Controllers;
 
 use Clases\Email;
 use Model\Usuario;
 use MVC\Router;
 
-class LoginController {
-    public static function login(Router $router) {
-        $router->render('auth/login');
+class LoginController
+{
+    public static function login(Router $router)
+    {
+        $alertas = [];
+        $auth = new Usuario;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $auth = new Usuario($_POST);
+            
+            //Validar
+            $alertas = $auth->validarLogin();
+
+            if(empty($alertas)) {
+                //Comprobar que exista el usuario
+                $usuario = Usuario::where('email', $auth->email);
+                
+                if($usuario) {
+                    //Verfico el password
+                    if($usuario->comprobarPasswordAndVerificado($auth->password)) {
+                        // Autenticar el usuario
+                        session_start();
+
+                        $_SESSION['id'] = $usuario->id;
+                        $_SESSION['nombre'] = $usuario->nombre . " " . $usuario->apellido;
+                        $_SESSION['email'] = $usuario->email;
+                        $_SESSION['login'] = true;
+
+                        // Redireccionamiento
+                        if($usuario->admin === "1") {
+                            $_SESSION['admin'] = $usuario->admin ?? null;
+                            
+                            header('Location: /admin');
+                        } else {
+                            header('Location: /cita');
+                        }                        
+
+
+                    }
+                } else {
+                    Usuario::setAlerta('error', 'Usuario no encontrado');
+                }
+            }
+        }
+        $alertas = Usuario::getAlertas();
+        $router->render('auth/login', [
+            'alertas' => $alertas,
+            'auth' => $auth
+        ]);
     }
-    public static function logout() {
+    public static function logout()
+    {
         echo "Desde logout";
     }
-    public static function olvide(Router $router) {
+    public static function olvide(Router $router)
+    {
         $router->render('auth/olvide-password');
     }
-    public static function recuperar() {
+    public static function recuperar()
+    {
         echo "Desde recuperar";
     }
-    public static function crear(Router $router) {
+    public static function crear(Router $router)
+    {
 
         $usuario = new Usuario;
 
         //Alertas vacias
         $alertas = [];
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST') { 
-    
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
             $usuario->sincronizar($_POST); //iteran los datos enviados y sincroniza el obj vacio con los nuevos datos
             $alertas = $usuario->validarNuevaCuenta();
 
             //Revisar que alerta este vacio
-            if(empty($alertas)) {
+            if (empty($alertas)) {
                 // Verificar que el usuario no este registrado
                 $resultado = $usuario->existeUsuario();
 
-                if($resultado->num_rows) { 
+                if ($resultado->num_rows) {
                     $alertas = Usuario::getAlertas();
                 } else {
                     // Hashear el Password
                     $usuario->hashPassword();
-                    
+
                     // Generar un Token unico
                     $usuario->crearToken();
-                    
+
                     // Enviar el email
                     $email = new Email($usuario->nombre, $usuario->email, $usuario->token);
                     $email->enviarConfirmacion();
-                    
+
                     //Crear el usuario
                     $resultado = $usuario->guardar();
                     //debuguear($resultado);
-                    if($resultado) {
+                    if ($resultado) {
                         header('Location: /mensaje');
                     }
-                    
                 }
             }
-
         }
 
         $router->render('auth/crear-cuenta', [
@@ -65,7 +114,33 @@ class LoginController {
             'alertas' => $alertas
         ]);
     }
-    public static function mensaje(Router $router) {
+    public static function mensaje(Router $router)
+    {
         $router->render('auth/mensaje');
+    }
+    public static function confirmar(Router $router)
+    {
+
+        $alertas = [];
+
+        $token = s($_GET['token']);
+        $usuario = Usuario::where('token', $token); //si no hay token es NULL
+
+        if (empty($usuario)) {
+            // Mensaje de error
+            Usuario::setAlerta('error', 'Token No Valido');
+        } else {
+            // Modificar a un usuario confirmado
+            $usuario->confirmado = "1";
+            $usuario->token = NULL; //se borra ya que se habria confirmado el usuario y no hace falta mas el token
+            $usuario->guardar();
+            Usuario::setAlerta('exito', 'Cuenta Comprobada Correctamente');
+        }
+
+        // Leer alertas antes de renderizar las vistas
+        $alertas = Usuario::getAlertas();
+        $router->render('auth/confirmar-cuenta', [
+            'alertas' => $alertas
+        ]);
     }
 }
--- models/Usuario.php
+++ models/Usuario.php
@@ -55,6 +55,16 @@ class Usuario extends ActiveRecord
         return self::$alertas;
     }
 
+    public function validarLogin() {
+        if (!$this->email) {
+            self::$alertas['error'][] = 'El Email es Obligatorio';
+        }
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password es Obligatorio';
+        }
+        return self::$alertas;
+    }
+
     public function existeUsuario()
     {
         $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
@@ -72,5 +82,15 @@ class Usuario extends ActiveRecord
     }
     public function crearToken() {
         $this->token = uniqid();
+    }
+    public function comprobarPasswordAndVerificado($password){
+        
+        $resultado = password_verify($password,$this->password);
+
+        if(!$resultado || !$this->confirmado) {
+            self::$alertas['error'][] = 'Password Incorrecto o tu cuenta no ha sido confirmada';
+        } else {
+            return true;
+        }
     }
 }
